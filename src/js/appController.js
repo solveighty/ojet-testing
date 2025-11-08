848153ca4fecb280fb7a0317ddf9c491d072cb65
solveighty <solveighty@gmail.com>
2025-11-08 18:24:12 -0500

    🎨 ui(ticket-desk): Actualizar estilos y estructura del contenedor de tickets para mejorar la usabilidad y la experiencia visual.

--- src/js/appController.js
+++ src/js/appController.js
@@ -67,19 +67,10 @@ define(['knockout', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojknocko
       }
 
       // Header
-      // Application Name used in Branding Area
-      this.appName = ko.observable("App Name");
+      // 🎨 APP NAME - Nombre de la aplicación actualizado
+      this.appName = ko.observable("My Online Support");
       // User Info used in Global Navigation area
       this.userLogin = ko.observable("john.hancock@oracle.com");
-
-      // Footer
-      this.footerLinks = [
-        {name: 'About Oracle', linkId: 'aboutOracle', linkTarget:'http://www.oracle.com/us/corporate/index.html#menu-about'},
-        { name: "Contact Us", id: "contactUs", linkTarget: "http://www.oracle.com/us/corporate/contact/index.html" },
-        { name: "Legal Notices", id: "legalNotices", linkTarget: "http://www.oracle.com/us/legal/index.html" },
-        { name: "Terms Of Use", id: "termsOfUse", linkTarget: "http://www.oracle.com/us/legal/terms/index.html" },
-        { name: "Your Privacy Rights", id: "yourPrivacyRights", linkTarget: "http://www.oracle.com/us/legal/privacy/index.html" },
-      ];
      }
      // release the application bootstrap busy state
      Context.getPageContext().getBusyContext().applicationBootstrapComplete();
